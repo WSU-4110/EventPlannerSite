1e4505feb9cb67c2b96df637199ad1e0aab7fc6c
Jibran Iqbal <hk9822@wayne.edu>
2025-03-14 18:42:12 -0400

    replace old files with new in main branch

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-import { EventDataSourceStrategy, HardcodedEventDataSourceStrategy } from './eventDataSourceStrategies.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const dataSource = new HardcodedEventDataSourceStrategy();
-
-    const calendarConfigurator = new CalendarConfigurator(dataSource);
-
-    calendarConfigurator.initializeCalendar();
-});
-
-class CalendarConfigurator {
-    constructor(dataSourceStrategy) {
-        this.dataSourceStrategy = dataSourceStrategy;
-    }
-
-    initializeCalendar() {
-        const { Calendar } = window.VanillaCalendarPro;
-
-        const calendar = new Calendar('#calendar', {
-            settings: {
-                iso8601: false, 
-            },
-            events: this.dataSourceStrategy.getEvents(), 
-        });
-        calendar.init();
-    }
+// Script from login.html
+function showRegisterForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
 }
+
+function showLoginForm() {
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
+// Script from planner.html
+
+// Initially show the "View Events" section or any default section
