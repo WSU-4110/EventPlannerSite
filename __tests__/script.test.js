effe8e61891545efffd50c732b328118c5b87198
Jibran Iqbal <hk9822@wayne.edu>
2025-04-03 23:26:11 -0400

    unit test 6

--- __tests__/script.test.js
+++ __tests__/script.test.js
@@ -1,8 +1,8 @@
-it('loads public and invited events for the user', async () => {
-    getDocs.mockResolvedValueOnce({ forEach: () => {} }); // public
-    getDocs.mockResolvedValueOnce({ forEach: () => {} }); // private
-    await getAllEvents();
-    expect(getDocs).toHaveBeenCalledTimes(2);
+it('records RSVP for a user', async () => {
+    setDoc.mockResolvedValue();
+    await rsvpToEvent('event456', 'user789');
+    expect(setDoc).toHaveBeenCalled();
   });
+  
   
   
