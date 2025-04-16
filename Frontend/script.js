0eb1bede5c0e9e059ab3a664a9390c4cdacd25b4
Jibran Iqbal <hk9822@wayne.edu>
2025-04-16 15:07:43 -0400

    add all the feedback to the code

--- Frontend/script.js
+++ Frontend/script.js
@@ -306,9 +306,8 @@ window.editEvent = async function(eventId) {
               <input type="file" id="gallery-input-${eventId}" multiple>
               <button onclick="window.uploadGalleryImages('${eventId}')">Upload</button>
               ${eventData.gallery ? eventData.gallery.map(url => `<img src="${url}" style="max-width: 100px; margin: 5px;">`).join('') : ''}
-              <br><button onclick="document.getElementById('feedback-list-${eventId}').style.display='block'">View Feedback</button>
+              <br><button onclick="window.toggleFeedback('${eventId}')">View Feedback</button>
               <div id="feedback-list-${eventId}" style="margin-top:10px; display:none;"></div>
-              <script>window.loadFeedbackForEvent('${eventId}');</script>
             </li>`;
           });
           html += '</ul>';
@@ -322,6 +321,7 @@ window.editEvent = async function(eventId) {
       myEventsList.innerHTML = '<p>Failed to load your events.</p>';
     }
   };
+  
   
   
   
