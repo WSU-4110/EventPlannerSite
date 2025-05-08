6649a857fb922cd1af501d2a2fa8e5fa15b62ec5
Jibran Iqbal <86741868+scalenetoast@users.noreply.github.com>
2025-05-08 17:46:54 -0400

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -8,13 +8,6 @@ import { getFirestore, collection, doc, setDoc, addDoc, getDocs, getDoc, query, 
 
 //firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyB78Zxb1syQWWtf-bPDzGdhClkmBZsKkWM",
-    authDomain: "eventplannersite-7e4a1.firebaseapp.com",
-    projectId: "eventplannersite-7e4a1",
-    storageBucket: "eventplannersite-7e4a1.firebasestorage.app",
-    messagingSenderId: "107253360225",
-    appId: "1:107253360225:web:7cbdea05d0d78e68576c20",
-    measurementId: "G-JNNSMNC46Y"
 };
 
 const app = initializeApp(firebaseConfig);
